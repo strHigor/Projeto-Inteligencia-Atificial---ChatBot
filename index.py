61b91d156ab996713bceb1c5eb3d84eb7c57b61f
Higor <higor.b97@hotmail.com>
2022-11-19 16:13:58 -0300

    .

--- index.py
+++ index.py
@@ -1,30 +1,21 @@
 #Integrantes: Higor Barbosa, Fillipe Cabral, Mateus Basilio
+from chatterbot.trainers import ListTrainer 
+from chatterbot.trainers import ChatterBotCorpusTrainer 
 from chatterbot import ChatBot 
-from chatterbot.trainers import ListTrainer 
-from chatterbot.trainers import ListTrainer 
 
-bot = ChatBot('Bot') 
-  
-trainer = ListTrainer(bot) 
-  
-trainer.train([ 
-    'Hi', 
-    'Hello', 
-    'I need roadmap for Competitive Programming', 
-    'Just create an account on GFG and start', 
-    'I have a query.', 
-    'Please elaborate, your concern', 
-    'How long it will take to become expert in Coding ?', 
-    'It usually depends on the amount of practice.', 
-    'Ok Thanks', 
-    'No Problem! Have a Good Day!'
-]) 
-  
-while True: 
-    request=input('you :') 
-    if request == 'OK' or request == 'ok': 
-        print('Bot: bye') 
-        break
-    else: 
-        response=bot.get_response(request) 
-        print('Bot:', response) 
+bot = ChatBot('Pegasus')
+conversa = ChatterBotCorpusTrainer(bot)
+conversa = ListTrainer(bot)
+
+conversa.train([
+     'Quem é você?', 'Eu sou'
+])
+
+print("####Início da interação do Chatbot####")
+while True:
+    pergunta = input("Usuário: ")
+    resposta = bot.get_response(pergunta)
+    if float(resposta.confidence) > 0.5:
+        print("Pégasus: ", resposta)
+    else:
+        print('Pégasus: Ainda não sei responder esta pergunta.')
